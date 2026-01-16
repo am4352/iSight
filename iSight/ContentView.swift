e9c1b1a8f446a955d8b7161c1126abd601815613
Anuj Mishra <am4352@srmist.edu.in>
2026-01-16 22:49:26 +0530

    Initial Commit

---
+++ iSight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSight
+//
+//  Created by Anuj   on 16/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSight/iSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSightApp.swift
+//  iSight
+//
+//  Created by Anuj   on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct iSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
